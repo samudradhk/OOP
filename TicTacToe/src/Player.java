2a14c667903fe364393da481b939243cb125e0d8
samudradhk <samudradhika@gmail.com>
2025-02-13 09:02:50 +0700

    push TicTacToe

---
+++ TicTacToe/src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    public char symbol;
+    
+    public Player(char symbol){
+        this.symbol = symbol;
+    }
+    
+    public char getsymbol(){
+        return symbol;
+    }
+}
---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Board board;
+    private Player player1,player2;
+    private Scanner scanner;
+    
+    public TicTacToe(){
+        board = new Board();
+        player1 = new Player('O');
+        player2 = new Player('X');
+        scanner = new Scanner(System.in);
+    }
+    
+    public void startGame(){
+        System.out.println("Selamat Datang di TicTacToe");
+        board.printBoard();
+        Player currentPlayer = player1;
+        
+        while(true){
+            System.out.print("Player " + currentPlayer.getsymbol() + " move[row][col]: ");
+            int row = scanner.nextInt() -1;
+            int col = scanner.nextInt() -1;
+            
+            if(board.makeMove(row, col, currentPlayer.getsymbol())){
+                board.printBoard();
+                if(board.checkWin(currentPlayer.getsymbol())){
+                    System.out.println("Player "+ currentPlayer.getsymbol()+ " wins!!!");
+                    break;
+                }else if(board.isFull()){
+                    System.out.println("tidak ada yang menang, silahkan coba lagi!!");
+                    break;
+                }
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
+            }else{
+                System.out.println("langkah yang salah, silahkan coba lagi");
+            }
+        }
+        scanner.close();
+    }
+}
